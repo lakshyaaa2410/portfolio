1d850a744a4eb40980e650ec610ca472eac6d412
Lakshya Choudhary <lakshyachoudhary1@gmail.com>
2025-11-05 22:53:46 +0530

    Changes in Landing section

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,34 +8,45 @@ import AvatarMorph from "./utils/AvatarMorph";
 
 function Landing() {
 	return (
-		<div className="grid grid-cols-2 mt-10">
-			<div className="text-center flex items-center justify-center">
-				<div>
-					<h1
-						className="text-5xl text-wrap italic"
-						style={{ fontFamily: "'Press Start 2P', cursive" }}
-					>
-						Design. Develop. Deploy. Deliver.
-					</h1>
-					<p
-						className="mt-10 text-sm italic"
-						style={{ fontFamily: "'Press Start 2P', cursive" }}
-					>
-						From design to deployment through clean engineering.
-					</p>
-					<div className="mt-10 flex flex-row space-x-8 justify-center">
-						<LinkedIn pop="#0a66c2" height="35px" />
-						<Leetcode pop="#f89d17" height="35px" />
-						<Github pop="white" height="35px" />
-						<Instagram pop="#ff0e5b" height="35px" />
-						<Gmail pop="red" height="35px" />
+		<>
+			<div className="grid grid-cols-2 mt-10">
+				<div className="text-center flex items-center justify-center">
+					<div>
+						<h1
+							className="text-3xl text-wrap italic"
+							style={{ fontFamily: "'Press Start 2P', cursive" }}
+						>
+							Design. Develop. Deploy. Deliver.
+						</h1>
+						<p
+							className="mt-10 text-sm/8 italic"
+							style={{ fontFamily: "'Press Start 2P', cursive" }}
+						>
+							I'm Lakshya, Full-Stack Developer and DevOps
+							Engineer who turns ideas into production-ready apps.
+						</p>
+						<div className="flex flex-row justify-center gap-10 mt-10 text-white">
+							<button className="bg-white text-[#0d1c2f] px-6 py-1.5 rounded-md italic cursor-pointer">
+								Resume
+							</button>
+							<button className="border-white border-2 px-6 py-1.5 rounded-md hover:bg-white hover:text-[#0d1c2f] cursor-pointer">
+								Contact
+							</button>
+						</div>
+						<div className="mt-10 flex flex-row space-x-8 justify-center">
+							<LinkedIn pop="#0a66c2" height="27.5px" />
+							<Leetcode pop="#f89d17" height="27.5px" />
+							<Github pop="white" height="27.5px" />
+							<Instagram pop="#ff0e5b" height="27.5px" />
+							<Gmail pop="red" height="27.5px" />
+						</div>
 					</div>
 				</div>
+				<div className="flex justify-end">
+					<AvatarMorph />
+				</div>
 			</div>
-			<div className="flex justify-end">
-				<AvatarMorph />
-			</div>
-		</div>
+		</>
 	);
 }
 
